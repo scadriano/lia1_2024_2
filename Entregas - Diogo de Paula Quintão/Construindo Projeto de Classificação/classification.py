b399c31cd32a2935419b1e30e86b23275de67bc6
Diogo Quintão <quintaodiogo@gmail.com>
2024-10-07 16:49:41 -0300

    Adicionando projeto da aula 07/10

--- Entregas - Diogo de Paula Quintão/Construindo Projeto de Classificação/classification.py
+++ Entregas - Diogo de Paula Quintão/Construindo Projeto de Classificação/classification.py
@@ -5,7 +5,7 @@ from ultralytics import YOLO
 model = YOLO('model/yolo11n-cls.pt')
 
 # Ler a imagem de entrada
-image = cv2.imread('image/img01.png')
+image = cv2.imread('Entregas - Diogo de Paula Quintão\Construindo Projeto de Classificação\image\img03.png')
 
 # Realizar a predição na imagem
 result = model.predict(image, verbose=False)
